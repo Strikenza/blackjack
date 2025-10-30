ff14660e246d30432993478e2ca06008324e84c2
StevenTarasula <stevent190@nycstudents.net>
2025-10-30 13:19:54 -0400

    idk

--- src/Main.java
+++ src/Main.java
@@ -3,35 +3,46 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int card;
+        int money = 1000;
         List<Integer> deck = new newDeck().newDeck();
         List<String> names = new newDeck().name();
         boolean reset;
         while (1==1) {
             System.out.println("--------------------");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("How much would you like to bet?");
+            int bet = scanner.nextInt();
+            System.out.println("You bet $" + bet + "!");
             reset = false;
-            System.out.println(deck);
-            System.out.println(names);
             List<Integer> playerHand = new ArrayList<>();
             List<Integer> dealerHand = new ArrayList<>();
             List<String> playerHandNames = new ArrayList<>();
             List<String> dealerHandNames = new ArrayList<>();
+            deck = resetCheckDeck(deck);
+            names = resetCheckNames(names);
             card = returnCard(deck.size());
             dealerHand.add(deck.get(card));
             dealerHandNames.add(names.get(card));
             deck.remove(card);
             names.remove(card);
+            deck = resetCheckDeck(deck);
+            names = resetCheckNames(names);
             card = returnCard(deck.size());
             playerHand.add(deck.get(card));
             playerHandNames.add(names.get(card));
             deck.remove(card);
             names.remove(card);
+            deck = resetCheckDeck(deck);
+            names = resetCheckNames(names);
             card = returnCard(deck.size());
             dealerHand.add(deck.get(card));
             dealerHandNames.add(names.get(card));
             deck.remove(card);
             names.remove(card);
+            deck = resetCheckDeck(deck);
+            names = resetCheckNames(names);
             card = returnCard(deck.size());
             playerHand.add(deck.get(card));
             playerHandNames.add(names.get(card));
@@ -42,70 +53,80 @@ public class Main {
             System.out.println("Your total is " + total(playerHand));
             playerHand = aceChecker(playerHand);
             dealerHand = aceChecker((dealerHand));
-            Scanner scanner = new Scanner(System.in);
+            Scanner scanner1 = new Scanner(System.in);
             while (bust(total(playerHand)) == false) {
                 if (reset) {
-                    System.out.println("yo");
                     break;
                 }
                 else {
-                    System.out.println("gurt");
                     System.out.println("Would you like to hit or stand?");
-                    String hitOrStand = scanner.nextLine();
+                    String hitOrStand = scanner1.nextLine();
                     if (hitOrStand.equals("hit") || hitOrStand.equals("Hit")) {
+                        deck = resetCheckDeck(deck);
+                        names = resetCheckNames(names);
                         card = returnCard(deck.size());
                         playerHand.add(deck.get(card));
                         playerHandNames.add(names.get(card));
                         deck.remove(card);
                         names.remove(card);
                         System.out.println("You pulled " + playerHandNames.get(playerHandNames.size() - 1));
+                        Thread.sleep(1000);
                         playerHand = aceChecker(playerHand);
                         System.out.println("Your total is " + total(playerHand));
+                        Thread.sleep(1000);
                         if (bust(total(playerHand))) {
                             System.out.println("You busted with a " + total(playerHand) + ". You lose!");
+                            Thread.sleep(3000);
                             break;
                         }
                     } else {
+                        Thread.sleep(1000);
                         System.out.println("Dealer's face down card was " + dealerHandNames.get(1) + " and as such has a total of " + total(dealerHand));
                         while (bust(total(dealerHand)) == false) {
                             if (total(dealerHand) < 17) {
+                                deck = resetCheckDeck(deck);
+                                names = resetCheckNames(names);
                                 card = returnCard(deck.size());
                                 dealerHand.add(deck.get(card));
                                 dealerHandNames.add(names.get(card));
                                 deck.remove(card);
                                 names.remove(card);
                                 System.out.println("The dealer pulled " + dealerHandNames.get(dealerHandNames.size() - 1));
+                                Thread.sleep(1000);
                                 dealerHand = aceChecker(dealerHand);
                                 System.out.println("The dealer's total is " + total(dealerHand));
+                                Thread.sleep(1000);
                                 if (bust(total(dealerHand))) {
                                     System.out.println("Dealer busts with a " + total(dealerHand) + ". You win!");
+                                    Thread.sleep(3000);
                                     break;
                                 }
                             } else {
                                 if (!bust(total(dealerHand))) {
                                     if (total(playerHand) > (total(dealerHand))) {
                                         System.out.println("You win with a " + total(playerHand) + "!");
-                                        reset = true;
+                                        Thread.sleep(3000);
                                         break;
                                     }
                                     if (total(dealerHand) > (total(playerHand))) {
                                         System.out.println("Dealer wins with a " + total(dealerHand) + "!");
-                                        reset = true;
+                                        Thread.sleep(3000);
                                         break;
                                     }
                                     if (total(playerHand) == total(dealerHand)) {
                                         System.out.println("You tie! You and dealer have the same total of " + total(playerHand) + "!");
-                                        reset = true;
+                                        Thread.sleep(3000);
                                         break;
                                     }
                                 }
                                 if (bust(total(dealerHand))) {
                                     System.out.println("Dealer busts with a " + total(dealerHand) + ". You win!");
-                                    reset = true;
+                                    Thread.sleep(3000);
                                     break;
                                 }
                             }
                         }
+                        reset = true;
 
 
                     }
@@ -120,10 +141,6 @@ public class Main {
     }
     public static int returnCard(int decksize) {
         Random rng = new Random();
-        if (decksize <= 1) {
-            List<Integer> deck = new newDeck().newDeck();
-            List<String> names = new newDeck().name();
-        }
         int min = 0;
         int max = decksize - 1;
         return(rng.nextInt((max-min) + 1) + min);
@@ -157,4 +174,14 @@ public class Main {
             }
         return hand;
     }
+    public static List<Integer> resetCheckDeck(List<Integer> deck) {
+        if (deck.size() <= 1) return new newDeck().newDeck();
+        else return deck;
+    }
+    public static List<String> resetCheckNames(List<String> names) {
+        if (names.size() <= 1) return new newDeck().name();
+        else return names;
+
+    }
+
     }
